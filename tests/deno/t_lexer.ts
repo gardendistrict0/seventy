8cc736fccbfa24de0aea1048c1d314af5a9cdd5f
Garden <gardendistrict0x0@outlook.com>
2025-03-13 08:26:30 +1100

    FT: Add more tests

--- tests/deno/t_lexer.ts
+++ tests/deno/t_lexer.ts
@@ -2,11 +2,41 @@ import { assertEquals } from "https://deno.land/std@0.224.0/testing/asserts.ts";
 import { Lexer } from "../../lexer/lexer.ts";
 import { TokenType } from "../../global/global.ts";
 
-Deno.test("Lexer test", () => {
+Deno.test("Number/Whitespace tests", () => {
     const lexer = new Lexer("123 456");
     const tokens = lexer.tokenize();
     assertEquals(tokens, [
         { type: TokenType.INTEGER, value: "123" },
         { type: TokenType.INTEGER, value: "456" }
+    ]);
+});
+
+Deno.test("String tests", () => {
+    const lexer = new Lexer('"Hello, World!"');
+    const tokens = lexer.tokenize();
+    assertEquals(tokens, [
+        { type: TokenType.STRING, value: "Hello, World!" }
+    ]);
+});
+
+Deno.test("Operator tests", () => {
+    const lexer = new Lexer("+-*/=.");
+    const tokens = lexer.tokenize();
+    assertEquals(tokens, [
+        { type: TokenType.ADDITION, value: "+" },
+        { type: TokenType.SUBTRACTION, value: "-" },
+        { type: TokenType.MULTIPLY, value: "*" },
+        { type: TokenType.DIVIDE, value: "/" },
+        { type: TokenType.EQUAL, value: "=" },
+        { type: TokenType.ACCESS_PERIOD, value: "." }
+    ]);
+});
+
+Deno.test("Comment tests", () => {
+    const lexer = new Lexer("# This is a comment\n123");
+    const tokens = lexer.tokenize();
+    assertEquals(tokens, [
+        { type: TokenType.NEWLINE, value: "\n" },
+        { type: TokenType.INTEGER, value: "123" }
     ]);
 });
