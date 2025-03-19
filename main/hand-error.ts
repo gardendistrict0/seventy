f0522fbdd6c1e6f77160ed43c2ee2158a080b9ca
Garden <gardendistrict0x0@outlook.com>
2025-03-19 16:14:51 +1100

    FT: Make CLI better

--- main/hand-error.ts
+++
@@ -1,6 +0,0 @@
-export const ERRORS = {
-    SUCCESS: 0,
-    INVALID_FILE_PATH: 1,
-    LEX_ERROR: 2,
-    INVALID_ARG: 3,
-}
--- main/seventy.ts
+++ main/seventy.ts
@@ -1,14 +1,72 @@
 import { Lexer } from "../lexer/lexer.ts";
-const args : string[] = Deno.args;
 
-if (args.includes('--version')) {
-    console.log('v0.1.0-alpha'); // Print version
-    Deno.exit(0);
-} else if (args.includes('--tokenize')) {
-    const lexer = new Lexer(args[1]);
-    lexer.tokenize();
-    console.log(lexer.returnTokens());
-    Deno.exit(0);
-} else {
-    Deno.exit(1);
+const ARGS : string[] = Deno.args;
+const HELP_MESSAGE = `
+Usage: seventy [arguments]
+
+Supported arguments:
+    -h, --help                   Prints this help message
+    -v, --version                Prints the current version
+    -t [filePath], --tokenize [filePath]  
+                                  Tokenizes the given file
+`;
+
+const VERSION : string = "Seventy v0.1.0-alpha\n";
+let _silent : boolean = false;
+
+function silentCommand() {
+    _silent = true;
+}
+
+function printHelpMessage(helpMessage: string) {
+    console.log(helpMessage);
+}
+
+function printVersion(version: string) {
+    console.log(version);
+}
+
+function tokenizeArgument(filePath?: string) {
+    if (!filePath) {
+        console.error("ERROR: Missing file path.");
+        Deno.exit(1);
+    }
+
+    try {
+        const content : string = Deno.readTextFileSync(filePath);
+        const lexer = new Lexer(content);
+        console.log(lexer.tokenize());
+        Deno.exit(0);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error("ERROR: Failed to tokenize file:", error.message);
+        } else {
+            console.error("ERROR: Failed to tokenize file:", error);
+        }
+        Deno.exit(1);
+    }
+}
+
+
+if (ARGS.includes("-S") || ARGS.includes("--silent")) {
+    silentCommand();
+}
+
+
+switch (ARGS[0]) {
+    case '-H':
+    case '--help':
+        printHelpMessage(HELP_MESSAGE);
+        break;
+    case '-V':
+    case '--version':
+        printVersion(VERSION);
+        break;
+    case '-T':
+    case '--tokenize':
+        tokenizeArgument(ARGS[1]);
+        break;
+    default:
+        console.error("Use -H or --help to print supported commands.");
+        Deno.exit(1);
 }
