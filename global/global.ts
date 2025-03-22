b7ced4ba24ae07e45939114891e4095ca1ee8759
Garden <gardendistrict0x0@outlook.com>
2025-03-22 13:45:51 +1100

    FT: Add parser node templates for parser

--- global/global.ts
+++ global/global.ts
@@ -100,3 +100,88 @@ export const isLetter = (char : string) => {
 // Parser.ts
 //--------------------------------------------------------------------------------------------------------------
 
+// deno-lint-ignore no-explicit-any
+export function p_Literal(data: any) {
+    return {
+      type: "Literal",
+      value: data,
+      raw: `{${data}}`
+    };
+}
+
+export function p_Identifier(data: string) {
+    return {
+      type: "Identifier",
+      name: data,
+      raw: `{${data}}`
+    };
+}
+
+export function p_IdentifierPredef(data: string) {
+    return {
+      type: "IdentifierPredef",
+      name: data,
+      raw: `{${data}}`
+    };
+}
+
+// deno-lint-ignore no-explicit-any
+export function p_BinaryExpression(l_data: string, op: string, r_data: any) {
+    return {
+        type: "BinaryExpression",
+        left: l_data,
+        right: r_data,
+        operator: op,
+        raw: `${l_data} ${op} ${r_data}`
+        
+    }
+}
+
+// deno-lint-ignore no-explicit-any
+export function p_UnaryExpression(op: string, data: any) {
+    return {
+        type: "UnaryExpression",
+        operator: op,
+        argument: data,
+        raw: `${op} ${data}`
+    }
+}
+
+// deno-lint-ignore no-explicit-any
+export function p_AssignmentExpression(id: string, op: string, data: any) {
+    return {
+        type: "AssignmentExpression",
+        left: id,
+        right: data,
+        operator: op,
+        raw: `${id} ${op} ${data}`
+    }
+}
+
+export function p_CallExpression(func: string, args: string[]) {
+    return {
+        type: "CallExpression",
+        left: func,
+        right: args,
+        raw: `${func}(${args.join(", ")})`
+    }
+}
+
+export function p_ObjectAccess(obj: string, property: string) {
+    return {
+        type: "ObjectAccess",
+        left: obj,
+        right: property,
+        raw: `${obj}.${property}`
+    }
+}
+
+export function p_ArrayAccess(array: string, id: string) {
+    return {
+        type: "ArrayAccess",
+        left: array,
+        right: id,
+        raw: `${array}[${id}]`
+    }
+}
+
