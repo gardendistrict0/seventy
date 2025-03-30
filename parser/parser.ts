1e1682bd1946bfd194bc5d36408c7e0a22ae0c5f
Garden <gardendistrict0x0@outlook.com>
2025-03-30 13:03:14 +1100

    FT: Add binary expressions to parsing

--- parser/parser.ts
+++ parser/parser.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore no-unused-vars
-import { Token, TokenType } from "../global/global.ts";
+import { p_BinaryExpression, Token, TokenType,  } from "../global/global.ts";
   
 
 export class Parser{
@@ -9,12 +9,28 @@ export class Parser{
       this.Tokens = Tokens;
     }
 
-    setupAST() {
+    parse() {
         this.AST = {
             type: "Program",
             body: []
-        };
+        }
+
+        let index = 0;
+        while (index < this.Tokens.length) {
+            const token = this.Tokens[index];
+            const l_token = this.Tokens[index - 1];
+            const r_token = this.Tokens[index + 1];
+            if (token.type === TokenType.ADDITION) {
+                this.AST.body.push(p_BinaryExpression(l_token.value, "+", r_token.value));
+            } else if (token.type === TokenType.SUBTRACTION) {
+                this.AST.body.push(p_BinaryExpression(l_token.value, "-", r_token.value));
+            } else if (token.type === TokenType.MULTIPLY) {
+                this.AST.body.push(p_BinaryExpression(l_token.value, "*", r_token.value));
+            } else if (token.type === TokenType.DIVIDE) {
+                this.AST.body.push(p_BinaryExpression(l_token.value, "/", r_token.value));
+            }
+            index++;
+        }
     }
     
-
 }
